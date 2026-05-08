4c3d7c76c3f42c23c0dcebd90e9b9bf35fcc0762
Indraaai <iindra09323@gmail.com>
2026-05-08 21:31:43 +0700

    setup projects

---
+++ database/migrations/2026_05_08_142822_create_leave_requests_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leave_requests', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->unsignedInteger('total_days');
+
+            $table->text('reason');
+            $table->string('attachment_path');
+
+            $table->string('status')->default('pending');
+
+            $table->foreignId('reviewed_by')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete();
+
+            $table->timestamp('reviewed_at')->nullable();
+            $table->text('rejection_reason')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leave_requests');
+    }
+};
