7e7d67baa65023ef83b5fc684c14e7451cfbd954
Indraaai <iindra09323@gmail.com>
2026-05-11 13:32:52 +0700

    add admin seeder

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,15 @@ class AdminUserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::updateOrCreate(
+            ['email' => 'admin@gmail.com'],
+            [
+                'name' => 'Admin User',
+                'password' => 'password123',
+                'role' => 'admin',
+                'provider' => null,
+                'provider_id' => null,
+            ]
+        );
     }
 }
